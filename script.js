19d044bd66f21077a91fbd04a0df0c3592200369
MM07 <71704916+MM079487@users.noreply.github.com>
2021-10-25 16:03:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+var score = 0;
+
 function changePosition(){
+    score++
     const b = document.getElementById('b')//获取按钮id
-    let num = Math.floor((Math.random() * 90) + 10);//随机生成10 - 90的号码
+    const scoreEl = document.getElementById('score')
+    let num = Math.floor((Math.random() * 50) + 30);//随机生成30 - 80的号码
     let num2 = Math.floor((Math.random() * 50) + 30);
     b.style.top=`${num}%`;
     b.style.left=`${num2}%`
+    
+    scoreEl.innerText=`Score : ${score}`;
 }
